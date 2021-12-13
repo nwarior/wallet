ebd8bf30e563c56e5014e555ceec6c85ac20e8b7
Nurbek <samoindig@gmail.com>
2021-12-13 12:22:20 +0500

    фиксирован баг

--- cmd/main.go
+++ cmd/main.go
@@ -6,11 +6,13 @@ import (
 
 func main() {
 	s := &wallet.Service{}
+	/*
 	s.RegisterAccount("+992000000001")
 	s.RegisterAccount("+992000000002")
 	s.RegisterAccount("+992000000003")
 	s.RegisterAccount("+992000000004")
 	
 	s.ExportToFile("data/accounts.txt")
+	*/
 	s.ImportFromFile("data/accounts.txt")
 }
